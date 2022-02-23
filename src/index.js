edb1419b3c95eb517c10b4d058e1b59f15b7e0ea
miris-mimitako <miris.mikitako@gmail.com>
2022-02-23 21:55:06 +0900

    learn js 3-1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-const title1 = document.getElementsByClassName("title");
-console.log(title1);
+
+
+// # 3-1
+// const title1 = document.getElementsByClassName("title");
+// console.log(title1);
+
+// const containers = document.getElementsByClassName("container");
+// console.log(containers)
+
+// const container = document.querySelector(".container");
+// console.log(container)
 
 //console.log("text");
